06d04f4316b57ede4af3f685a9d543e50958d55c
michaelrmartinezm@gmail.com <michaelrmartinezm@gmail.com@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-03-23 15:31:18 +0000

    Creación del proyecto Actualización del cronograma

---
+++ trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.BC/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UPC.Proyecto.SISPPAFUT.BC
+{
+    public class Class1
+    {
+    }
+}
---
+++ trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.DALC/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UPC.Proyecto.SISPPAFUT.DALC
+{
+    public class Class1
+    {
+    }
+}
