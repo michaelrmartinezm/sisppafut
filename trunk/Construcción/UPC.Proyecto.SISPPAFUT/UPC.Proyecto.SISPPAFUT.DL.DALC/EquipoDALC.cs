e97445059dc3f2a2a32b4e927484976a244f5a44
renzo.martinezh <renzo.martinezh@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-03-29 21:03:30 +0000

    Implemetacion de User Story: Registrar Pais (DALC, BE, BC, UI) (Done)

--- trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.DL.DALC/EquipoDALC.cs
+++ trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.DL.DALC/EquipoDALC.cs
@@ -8,7 +8,7 @@ namespace UPC.Proyecto.SISPPAFUT.DL.DALC
     {
         public int InsertarEquipo()
         {
- 
+            return 0;
         }
     }
 }
---
+++ trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT/PaisInsertar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using UPC.Proyecto.SISPPAFUT.BL.BE;
+using UPC.Proyecto.SISPPAFUT.BL.BC;
+
+namespace UPC.Proyecto.SISPPAFUT
+{
+    public partial class PaisInsertar : Form
+    {
+        public PaisInsertar()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_registrar_Click(object sender, EventArgs e)
+        {
+            int iCodigo = 0;
+
+            PaisBE objPaidBE = new PaisBE();
+            PaisBC objPaisBC = new PaisBC();
+
+            objPaidBE.NombrePais = txt_pais.Text;
+
+            iCodigo = objPaisBC.insertarPais(objPaidBE);
+
+            if(iCodigo == -1)
+            {
+                MessageBox.Show("ESTE PAIS YA HA SIDO INGRESADO ANTERIORMENTE");
+            }
+
+            else if (iCodigo == 0)
+            {
+                MessageBox.Show("NO SE PUDO REGISTRAR ESTE PAIS");
+            }
+
+            else
+            {
+                MessageBox.Show("EL PAIS HA SIDO REGISTRADO SATISFACTORIAMENTE");
+            }
+        }
+    }
+}
