76b496a50833aac7927ea1991936aa399e26a5be
rodolfocc88@gmail.com <rodolfocc88@gmail.com@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-04-02 23:05:08 +0000

    Actualizacion de la tabla y archivos terminados

---
+++ Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.BL.BC/EquipoBC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using UPC.Proyecto.SISPPAFUT.BL.BE;
+using UPC.Proyecto.SISPPAFUT.DL.DALC;
+
+namespace UPC.Proyecto.SISPPAFUT.BL.BC
+{
+    public class EquipoBC
+    {
+        public int insertarEquipo(EquipoBE objEquipoBE)
+        {
+            EquipoDALC objEquipoDALC;
+
+            try
+            {
+                objEquipoDALC = new EquipoDALC();
+
+                if (objEquipoDALC.existe_Equipo(objEquipoBE.NombreEquipo) == 1)
+                {
+                    return -1;
+                }
+
+                return objEquipoDALC.insertar_Equipo(objEquipoBE);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public List<EquipoBE> listarEquipos()
+        {
+            EquipoDALC objEquipoDALC = new EquipoDALC();
+            return objEquipoDALC.listar_Equipos();
+        }
+    }
+}
