42373c1f1bed488b15cb0222d78a06c843b89298
rodolfocc88@gmail.com <rodolfocc88@gmail.com@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-04-02 23:05:08 +0000

    Actualizacion de la tabla y archivos terminados

---
+++ trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.BL.BC/LigaBC.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using UPC.Proyecto.SISPPAFUT.BL.BE;
+using UPC.Proyecto.SISPPAFUT.DL.DALC;
+
+namespace UPC.Proyecto.SISPPAFUT.BL.BC
+{
+    public class LigaBC
+    {
+        public int insertarLiga(LigaBE objLigaBE)
+        {
+            LigaDALC objLigaDALC;
+
+            try
+            {
+                objLigaDALC = new LigaDALC();
+
+                if (objLigaDALC.existe_Liga(objLigaBE.NombreLiga) == 1)
+                {
+                    return -1;
+                }
+
+                return objLigaDALC.insertar_Liga(objLigaBE);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public List<LigaBE> listarLigas()
+        {
+            LigaDALC objLigaDALC = new LigaDALC();
+            return objLigaDALC.listar_Ligas();
+        }
+
+        public void insertarEquipoEnLiga(int codigoLiga, int codigoEquipo)
+        {
+            LigaDALC objLigaDALC;
+
+            try
+            {
+                objLigaDALC = new LigaDALC();
+                objLigaDALC.insertar_equipoEnLiga(codigoLiga, codigoEquipo);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT/frmEquipoInsertar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using UPC.Proyecto.SISPPAFUT.BL.BE;
+using UPC.Proyecto.SISPPAFUT.BL.BC;
+
+namespace UPC.Proyecto.SISPPAFUT
+{
+    public partial class frmEquipoInsertar : Form
+    {
+        public frmEquipoInsertar()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            int iCodigo = 0;
+
+            EquipoBE objEquipoBE;
+            EquipoBC objEquipoBC;
+
+            objEquipoBE = new EquipoBE();
+            objEquipoBC = new EquipoBC();
+
+            //objEquipoBE.CodigoPais = 
+            objEquipoBE.NombreEquipo = txt_nombre.Text;
+            objEquipoBE.AnioFundacion = Convert.ToInt32(cmb_anio.SelectedValue);
+            objEquipoBE.CiudadEquipo = txt_nombre.Text;
+            //objEquipoBE.CodigoEstadioPrincipal =
+            //objEquipoBE.CodigoEstadioAlterno = 
+
+            iCodigo = objEquipoBC.insertarEquipo(objEquipoBE);
+
+            if (iCodigo == -1)
+            {
+                MessageBox.Show("ESTE EQUIPO YA HA SIDO INGRESADO ANTERIORMENTE");
+            }
+            else if (iCodigo == 0)
+            {
+                MessageBox.Show("NO SE PUDO REGISTRAR EL EQUIPO");
+            }
+            else
+            {
+                MessageBox.Show("EL EQUIPO SE REGISTRO SATISFACTORIAMENTE");
+            }
+        }
+    }
+}
