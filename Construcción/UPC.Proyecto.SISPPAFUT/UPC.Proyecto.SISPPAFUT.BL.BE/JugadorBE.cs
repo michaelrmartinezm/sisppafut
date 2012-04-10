7bc0af36daaa9becf6c8821c977ae67804c81b8f
renzo.martinezh <renzo.martinezh@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-04-10 23:54:33 +0000

    Implementacion de BE y DALC de US 020

---
+++ Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.BL.BE/JugadorBE.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UPC.Proyecto.SISPPAFUT.BL.BE
+{
+    public class JugadorBE
+    {
+        private int _codigoJugador;
+
+        public int CodigoJugador
+        {
+            get { return _codigoJugador; }
+            set { _codigoJugador = value; }
+        }
+
+        private int _codigoEquipo;
+
+        public int CodigoEquipo
+        {
+            get { return _codigoEquipo; }
+            set { _codigoEquipo = value; }
+        }
+
+        private String _nombres;
+
+        public String Nombres
+        {
+            get { return _nombres; }
+            set { _nombres = value; }
+        }
+
+        private String _apellidos;
+
+        public String Apellidos
+        {
+            get { return _apellidos; }
+            set { _apellidos = value; }
+        }
+
+        private String _nacionalidad;
+
+        public String Nacionalidad
+        {
+            get { return _nacionalidad; }
+            set { _nacionalidad = value; }
+        }
+
+        private DateTime _fechaNacimiento;
+
+        public DateTime FechaNacimiento
+        {
+            get { return _fechaNacimiento; }
+            set { _fechaNacimiento = value; }
+        }
+
+        private String _posicion;
+
+        public String Posicion
+        {
+            get { return _posicion; }
+            set { _posicion = value; }
+        }
+
+        private Decimal _altura;
+
+        public Decimal Altura
+        {
+            get { return _altura; }
+            set { _altura = value; }
+        }
+
+        private Decimal _peso;
+
+        public Decimal Peso
+        {
+            get { return _peso; }
+            set { _peso = value; }
+        }
+
+    }
+}
---
+++ Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.DL.DALC/JugadorDALC.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Data;
+using System.Data.SqlClient;
+using UPC.Proyecto.SISPPAFUT.BL.BE;
+
+namespace UPC.Proyecto.SISPPAFUT.DL.DALC
+{
+    public class JugadorDALC
+    {
+        public int insertar_Jugador(JugadorBE objJugadorBE)
+        {
+            SqlConnection conexion = null;
+            SqlCommand cmd_JugadorInsertar = null;
+
+            SqlParameter prm_CodigoJugador;
+            SqlParameter prm_CodigoEquipo;
+            SqlParameter prm_Nombres;
+            SqlParameter prm_Apellidos;
+            SqlParameter prm_Nacionalidad;
+            SqlParameter prm_Fecha;
+            SqlParameter prm_Posicion;
+            SqlParameter prm_Altura;
+            SqlParameter prm_Peso;
+
+            int iCodigoJugador;
+
+            String sqlJugadorInsertar;
+
+            try
+            {
+                conexion = new SqlConnection(Properties.Settings.Default.Cadena);
+
+                sqlJugadorInsertar = "spCreateJugador";
+
+                cmd_JugadorInsertar = new SqlCommand(sqlJugadorInsertar, conexion);
+                cmd_JugadorInsertar.CommandType = CommandType.StoredProcedure;
+
+                prm_CodigoJugador = new SqlParameter();
+                prm_CodigoJugador.Direction = ParameterDirection.ReturnValue;
+                prm_CodigoJugador.SqlDbType = SqlDbType.Int;
+
+                prm_CodigoEquipo = new SqlParameter();
+                prm_CodigoEquipo.ParameterName = "@CodEquipo";
+                prm_CodigoEquipo.SqlDbType = SqlDbType.Int;
+                prm_CodigoEquipo.Value = objJugadorBE.CodigoEquipo;
+
+                prm_Nombres = new SqlParameter();
+                prm_Nombres.ParameterName = "@Nombres";
+                prm_Nombres.SqlDbType = SqlDbType.VarChar;
+                prm_Nombres.Size = 20;
+                prm_Nombres.Value = objJugadorBE.Nombres;
+
+                prm_Apellidos = new SqlParameter();
+                prm_Apellidos.ParameterName = "@Apellidos";
+                prm_Apellidos.SqlDbType = SqlDbType.VarChar;
+                prm_Apellidos.Size = 20;
+                prm_Apellidos.Value = objJugadorBE.Apellidos;
+
+                prm_Nacionalidad = new SqlParameter();
+                prm_Nacionalidad.ParameterName = "@Nacionalidad";
+                prm_Nacionalidad.SqlDbType = SqlDbType.VarChar;
+                prm_Nacionalidad.Size = 20;
+                prm_Nacionalidad.Value = objJugadorBE.Nacionalidad;
+
+                prm_Fecha = new SqlParameter();
+                prm_Fecha.ParameterName = "@FechaNacimiento";
+                prm_Fecha.SqlDbType = SqlDbType.Date;
+                prm_Fecha.Value = objJugadorBE.FechaNacimiento;
+
+                prm_Posicion = new SqlParameter();
+                prm_Posicion.ParameterName = "@Posicion";
+                prm_Posicion.SqlDbType = SqlDbType.VarChar;
+                prm_Posicion.Size = 15;
+                prm_Posicion.Value = objJugadorBE.Posicion;
+
+                prm_Altura = new SqlParameter();
+                prm_Altura.ParameterName = "@Altura";
+                prm_Altura.SqlDbType = SqlDbType.Decimal;
+                prm_Altura.Value = objJugadorBE.Altura;
+
+                prm_Peso = new SqlParameter();
+                prm_Peso.ParameterName = "@Peso";
+                prm_Peso.SqlDbType = SqlDbType.Decimal;
+                prm_Peso.Value = objJugadorBE.Peso;
+
+                cmd_JugadorInsertar.Connection.Open();
+                cmd_JugadorInsertar.ExecuteNonQuery();
+
+                iCodigoJugador = Convert.ToInt32(prm_CodigoJugador.Value);
+
+                return iCodigoJugador;
+            }
+
+            catch (Exception)
+            {
+                if (conexion != null && conexion.State == ConnectionState.Open)
+                {
+                    conexion.Dispose();
+                }
+
+                throw;
+            }
+
+            finally
+            {
+                cmd_JugadorInsertar.Connection.Close();
+                conexion.Dispose();
+            }
+        }
+    }
+}
