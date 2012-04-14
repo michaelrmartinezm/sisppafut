0e9f80a1673c82e25b3711f6de7024326fcf5608
michaelrmartinezm@gmail.com <michaelrmartinezm@gmail.com@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-04-14 04:44:31 +0000

    Se ha actualizado el sp spListarEquipos para que envíe el valor '0' e aquellos casos en los que el campo CodEstadioAlterno sea Null.

--- Diseño/Base de Datos/store procedures/spListarEquipos.sql
+++ Diseño/Base de Datos/store procedures/spListarEquipos.sql
@@ -1,3 +1,13 @@
+USE [SISPPAFUT]
+GO
+
+/****** Object:  StoredProcedure [dbo].[spListarEquipos]    Script Date: 04/13/2012 23:24:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
 CREATE procedure [dbo].[spListarEquipos]
 (
 	@Pais varchar (20)
@@ -11,7 +21,10 @@ begin
 	e.AnioFundacion,
 	e.Ciudad,
 	e.CodEstadioPrincipal,
-	e.CodEstadioAlterno	
+	isNull(e.CodEstadioAlterno,0) as 'CodEstadioAlterno'
 	from Equipo e join Pais p on e.CodPais = p.CodPais
 	where p.Nombre = @Pais
 end
+GO
+
+
