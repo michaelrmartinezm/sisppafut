646def17b484686aa997bd9acdd6c185d2f476e9
renzo.martinezh <renzo.martinezh@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-04-16 23:06:17 +0000

    Update BC Project

---
+++ trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.BL.BC/JugadorBC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using UPC.Proyecto.SISPPAFUT.DL.DALC;
+using UPC.Proyecto.SISPPAFUT.BL.BE;
+
+namespace UPC.Proyecto.SISPPAFUT.BL.BC
+{
+    public class JugadorBC
+    {
+        public int insertar_Jugador(JugadorBE objJugadorBE)
+        {
+            JugadorDALC objJugadorDALC;
+
+            try
+            {
+                objJugadorDALC = new JugadorDALC();
+                return objJugadorDALC.insertar_Jugador(objJugadorBE);
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.BL.BC/PartidoBC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using UPC.Proyecto.SISPPAFUT.BL.BE;
+using UPC.Proyecto.SISPPAFUT.DL.DALC;
+
+namespace UPC.Proyecto.SISPPAFUT.BL.BC
+{
+    public class PartidoBC
+    {
+        public int insertar_Partido(PartidoBE objPartidoBE)
+        {
+            PartidoDALC objPartidoDALC;
+
+            try
+            {
+                objPartidoDALC = new PartidoDALC();
+                return objPartidoDALC.insertar_partido(objPartidoBE);
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
