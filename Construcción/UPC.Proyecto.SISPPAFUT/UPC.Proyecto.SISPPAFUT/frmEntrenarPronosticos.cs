b04ba1956b286e1090fb872b46286a4b8de00e77
rodolfocc88 <rodolfocc88@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-06-04 23:53:12 +0000

    Commit de archivos relacionados al Sprint 4

---
+++ Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT/frmEntrenarPronosticos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using UPC.Proyecto.SISPPAFUT.BL.BE;
+using UPC.Proyecto.SISPPAFUT.BL.BC;
+
+namespace UPC.Proyecto.SISPPAFUT
+{
+    public partial class frmEntrenarPronosticos : Form
+    {
+        //--Variables globales
+        List<PartidoBE> listaPartidos;
+        List<PronosticoBE> listaPronosticos;
+
+        private static frmEntrenarPronosticos frmEntrenamiento = null;
+        public static frmEntrenarPronosticos Instance()
+        {
+            if (frmEntrenamiento == null)
+            {
+                frmEntrenamiento = new frmEntrenarPronosticos();
+            }
+            return frmEntrenamiento;
+        }
+
+        public frmEntrenarPronosticos()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEntrenarPronosticos_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dg_PronosticosConfigurar();
+                dg_PronosticosDataBind();
+            }
+            catch (Exception ex)
+            {
+                Funciones.RegistrarExcepcion(ex);
+            }
+        }
+
+        private void dg_PronosticosConfigurar()
+        {
+            dg_Pronosticos.AllowUserToAddRows = false;
+            dg_Pronosticos.AllowUserToDeleteRows = false;
+
+            dg_Pronosticos.AllowUserToResizeColumns = false;
+            dg_Pronosticos.AllowUserToResizeRows = false;
+            dg_Pronosticos.AllowDrop = false;
+
+            dg_Pronosticos.MultiSelect = false;
+            dg_Pronosticos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            dg_Pronosticos.ReadOnly = true;
+        }
+
+        private void dg_PronosticosDataBind()
+        {
+            PartidoBC objPartidoBC = new PartidoBC();
+            PronosticoBC objPronosticoBC = new PronosticoBC();
+
+            listaPartidos = new List<PartidoBE>();
+            listaPronosticos = new List<PronosticoBE>();
+
+            listaPartidos = objPartidoBC.lista_todos_partidos();
+            listaPronosticos = objPronosticoBC.listar_Pronosticos();
+
+
+        }
+
+    }
+}
