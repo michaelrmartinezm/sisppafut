d41ade91d0da6abfb8cf8273d9a57d4bfab150dd
michaelrmartinezm@gmail.com <michaelrmartinezm@gmail.com@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-09-14 22:53:10 +0000

    Se ha actualizado la fecha de date->datetime. En el proyecto se deberán hacer los cambios correspondientes.

--- trunk/Diseño/Base de Datos/store procedures/spCreateJugador.sql
+++ trunk/Diseño/Base de Datos/store procedures/spCreateJugador.sql
@@ -1,7 +1,7 @@
 USE [SISPPAFUT]
 GO
 
-/****** Object:  StoredProcedure [dbo].[spCreateJugador]    Script Date: 04/21/2012 18:36:44 ******/
+/****** Object:  StoredProcedure [dbo].[spCreateJugador]    Script Date: 09/14/2012 17:34:59 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -13,7 +13,7 @@ CREATE PROCEDURE [dbo].[spCreateJugador]
 			@Nombres varchar(20)
            ,@Apellidos varchar(20)
            ,@Nacionalidad varchar(20)
-           ,@FechaNacimiento date
+           ,@FechaNacimiento datetime
            ,@Posicion varchar(15)
            ,@Altura numeric(5,2)
            ,@Peso numeric(5,2)
