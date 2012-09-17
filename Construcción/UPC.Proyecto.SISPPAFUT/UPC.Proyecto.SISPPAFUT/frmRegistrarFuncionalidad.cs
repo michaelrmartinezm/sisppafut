a4831d49225a796f28c383384ec0e580fdd3eaf0
milagroscruzd@gmail.com <milagroscruzd@gmail.com@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-09-17 05:34:15 +0000

    Se terminó con Registrar Funcionalidad, en todas sus capas mas su prueba unitaria. También se terminó con Registrar Rol, en todas sus capas mas su prueba unitaria.

---
+++ Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT/frmRegistrarFuncionalidad.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using UPC.Seguridad.BL.BC;
+
+namespace UPC.Proyecto.SISPPAFUT
+{
+    public partial class frmRegistrarFuncionalidad : Form
+    {
+        public frmRegistrarFuncionalidad()
+        {
+            InitializeComponent();
+        }
+
+        private static frmRegistrarFuncionalidad frmFuncionalidad = null;
+
+        public static frmRegistrarFuncionalidad Instance()
+        {
+            if (frmFuncionalidad == null)
+                frmFuncionalidad = new frmRegistrarFuncionalidad();
+
+            return frmFuncionalidad;
+        }
+
+        public bool ValidarControles()
+        {
+            if (txtNombreFuncionalidad.Text == " " || txtNombreFuncionalidad.Text.Length == 0 || txtDescripcionFuncionalidad.Text == " " || txtDescripcionFuncionalidad.Text.Length == 0)
+                return false;
+            return true;
+        }
+
+        public void LimpiarCampos()
+        {
+            txtDescripcionFuncionalidad.Clear();
+            txtNombreFuncionalidad.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ValidarControles() == false)
+            {
+                MessageBox.Show("Complete los campos corretamente");
+                return;
+            }
+
+            FuncionalidadBC objFuncionalidadBC;            
+
+            try
+            {
+                String Nombre = txtNombreFuncionalidad.Text;
+                String Descripcion = txtDescripcionFuncionalidad.Text;
+
+                objFuncionalidadBC = new FuncionalidadBC();
+
+                objFuncionalidadBC.Insertar_Funcionalidad(Nombre, Descripcion);
+
+                MessageBox.Show("Se registró la funcionalidad correctamente");
+
+                LimpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                Funciones.RegistrarExcepcion(ex);
+            }
+        }
+
+        private void frmRegistrarFuncionalidad_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Construcción/UPC.Proyecto.SISPPAFUT/UPC.Seguridad.DL.DALC/RolDALC.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Data.SqlClient;
+using UPC.Seguridad.BL.BE;
+
+namespace UPC.Seguridad.DL.DALC
+{
+    public class RolDALC
+    {
+        public int insertar_rol(RolBE objRolBE)
+        {
+            SqlConnection conexion = null;
+            SqlCommand cmd_rol_insertar = null;
+
+            SqlParameter prm_idRol;
+            SqlParameter prm_NombreRol;
+            SqlParameter prm_claveRol;
+            SqlParameter prm_descripcionRol;
+
+            int iCodigoRol;
+            String sqlRolInsertar;
+
+            try
+            {
+                conexion = new SqlConnection(Properties.Settings.Default.Cadena);
+
+                sqlRolInsertar = "spCreateRol";
+
+                cmd_rol_insertar = new SqlCommand(sqlRolInsertar, conexion);
+                cmd_rol_insertar.CommandType = CommandType.StoredProcedure;
+
+                prm_idRol = new SqlParameter();
+                prm_idRol.Direction = ParameterDirection.ReturnValue;
+                prm_idRol.SqlDbType = SqlDbType.Int;
+
+                prm_NombreRol = new SqlParameter();
+                prm_NombreRol.ParameterName = "@nombreRol";
+                prm_NombreRol.SqlDbType = SqlDbType.VarChar;
+                prm_NombreRol.Size = 30;
+                prm_NombreRol.Value = objRolBE.NombreRol;
+
+                prm_claveRol = new SqlParameter();
+                prm_claveRol.ParameterName = "@claveRol";
+                prm_claveRol.SqlDbType = SqlDbType.VarChar;
+                prm_claveRol.Size = 15;
+                prm_claveRol.Value = objRolBE.ClaveRol;
+
+                prm_descripcionRol = new SqlParameter();
+                prm_descripcionRol.ParameterName = "@descripcionRol";
+                prm_descripcionRol.SqlDbType = SqlDbType.VarChar;
+                prm_descripcionRol.Size = 200;
+                prm_descripcionRol.Value = objRolBE.DescripcionRol;
+
+                cmd_rol_insertar.Parameters.Add(prm_idRol);
+                cmd_rol_insertar.Parameters.Add(prm_NombreRol);
+                cmd_rol_insertar.Parameters.Add(prm_claveRol);
+                cmd_rol_insertar.Parameters.Add(prm_descripcionRol);
+
+                cmd_rol_insertar.Connection.Open();
+                cmd_rol_insertar.ExecuteNonQuery();
+
+                iCodigoRol = Convert.ToInt32(prm_idRol.Value);
+
+                return iCodigoRol;
+
+            }
+            catch (Exception ex)
+            {
+                if (conexion != null && conexion.State == ConnectionState.Open)
+                {
+                    conexion.Dispose();
+                }
+
+                throw;
+            }
+
+            finally
+            {
+                cmd_rol_insertar.Connection.Close();
+                conexion.Dispose();
+            }
+
+        }
+    }
+}
