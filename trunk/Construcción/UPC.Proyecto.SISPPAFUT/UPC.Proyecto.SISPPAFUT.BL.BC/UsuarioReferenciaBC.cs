314d69ad07182ca54faaca08490f248813b35209
rodolfocc88 <rodolfocc88@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-09-19 23:51:07 +0000

    actualizacion del sprint 5

---
+++ trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.BL.BC/UsuarioReferenciaBC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UPC.Proyecto.SISPPAFUT.DL.DALC;
+using UPC.Proyecto.SISPPAFUT.BL.BE;
+
+namespace UPC.Proyecto.SISPPAFUT.BL.BC
+{
+    public class UsuarioReferenciaBC
+    {
+        public void insertar_UsuarioReferencia(UsuarioReferenciaBE objUsuarioReferenciaBE)
+        {
+            UsuarioReferenciaDALC objUsuarioReferenciaDALC;
+
+            try
+            {
+                objUsuarioReferenciaDALC = new UsuarioReferenciaDALC();
+                objUsuarioReferenciaDALC.insertar_UsuarioReferencia(objUsuarioReferenciaBE);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
