75268ba317c8dc0d513997a7018840f532e138e8
milagroscruzd@gmail.com <milagroscruzd@gmail.com@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-09-23 04:44:03 +0000

    Se creó la funcionalidad UsuarioFuncionalidadBE.cs

---
+++ trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Seguridad.BL.BE/UsuarioFuncionalidadBE.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UPC.Seguridad.BL.BE
+{
+    public class UsuarioFuncionalidadBE
+    {
+        private int idusuario;
+
+        public int idUsuario
+        {
+            get { return idusuario; }
+            set { idusuario = value; }
+        }
+
+        private int idfuncionalidad;
+
+        public int idFuncionalidad
+        {
+            get { return idfuncionalidad; }
+            set { idfuncionalidad = value; }
+        }
+
+        private String nusuario;
+
+        public String Usuario
+        {
+            get { return nusuario; }
+            set { nusuario = value; }
+        }
+
+        private String nrol;
+
+        public String Rol
+        {
+            get { return nrol; }
+            set { nrol = value; }
+        }
+    }
+}
