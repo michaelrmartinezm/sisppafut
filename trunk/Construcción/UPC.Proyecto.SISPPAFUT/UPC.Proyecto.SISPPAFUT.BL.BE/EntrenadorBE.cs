d4e6a6b21ec67c9515c9bec16120dc5281967f44
milagroscruzd@gmail.com <milagroscruzd@gmail.com@31cc07bd-7986-a675-5af8-294cc2fd1d2d>
2012-10-22 21:29:23 +0000

    Se actualiza el Sprint 6

---
+++ trunk/Construcción/UPC.Proyecto.SISPPAFUT/UPC.Proyecto.SISPPAFUT.BL.BE/EntrenadorBE.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UPC.Proyecto.SISPPAFUT.BL.BE
+{
+    public class EntrenadorBE
+    {
+        private int codEntrenador;
+
+        public int CodEntrenador
+        {
+            get { return codEntrenador; }
+            set { codEntrenador = value; }
+        }
+
+        private int codEquipo;
+
+        public int CodEquipo
+        {
+            get { return codEquipo; }
+            set { codEquipo = value; }
+        }
+
+        private String nombres;
+
+        public String Nombres
+        {
+            get { return nombres; }
+            set { nombres = value; }
+        }
+
+        private String apellidos;
+
+        public String Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        private String nacionalidad;
+
+        public String Nacionalidad
+        {
+            get { return nacionalidad; }
+            set { nacionalidad = value; }
+        }
+
+        private DateTime fecha;
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+
+    }
+}
